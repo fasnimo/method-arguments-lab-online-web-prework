e6bf9e5e6444b4a04da4db689d1419a07cfc0bbd
Rene Cocom <fasnimo@gmail.com>
2019-03-12 21:45:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js}."
+  puts "Hi, my name is #{Josh} and I am learning to program in #{Ruby}."
 end 
